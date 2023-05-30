a5e3e447b4943ee010172a98522fff7618b47c27
byungmin12 <byunginkim12@gmail.com>
2023-05-30 21:56:26 +0900

    Sets widget
    상태 변경 추가
    container에서 버튼 위젯으로 변경

--- lib/widgets/Sets.dart
+++ lib/widgets/Sets.dart
@@ -6,44 +6,115 @@ class Sets extends StatefulWidget {
 }
 
 class _Sets extends State<Sets> {
-  int setCnt = 5;
+  int sets = 5;
+
+  void handlerUpdateSets(int newSets) {
+    setState(() {
+      sets = newSets;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.end,
       children: [
-        Container(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "$setCnt",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w900,
-                  fontSize: 24,
-                ),
+        ElevatedButton(
+          onPressed: () {
+            showModal(context, sets, handlerUpdateSets); // setCnt 값을 전달
+          },
+          style: ButtonStyle(
+              backgroundColor: MaterialStateProperty.resolveWith<Color?>(
+                    (Set<MaterialState> states) {
+                  return Colors.red; // 버튼의 색상 지정
+                },
               ),
-              const Text(
-                "SETS",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.w900,
-                ),
+              fixedSize: MaterialStateProperty.resolveWith<Size?>(
+                    (Set<MaterialState> states) {
+                  return const Size.square(100); // 버튼의 크기 지정
+                },
               ),
-            ],
-          ),
-          height: 100,
-          width: 100,
-          decoration: const BoxDecoration(
-            color: Colors.red,
-            borderRadius: BorderRadius.only(
-              bottomLeft: Radius.circular(19),
+              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                  const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(19),
+                    ),
+                  ))),
+          child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "$sets", // setCnt 값 사용
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w900,
+                    fontSize: 24,
+                  ),
+                ),
+                const Text(
+                  "SETS",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ],
             ),
           ),
         )
       ],
     );
   }
+}
+
+void showModal(BuildContext context, int sets, Function(int) handlerUpdateSets) {
+  showModalBottomSheet(
+    context: context,
+    builder: (BuildContext context) {
+      return StatefulBuilder(
+        builder: (BuildContext context, StateSetter setState) {
+          return Container(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text(
+                  'Sets: $sets',
+                  style: TextStyle(fontSize: 24.0),
+                ),
+                SizedBox(height: 16.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.arrow_upward),
+                      onPressed: () {
+                        setState(() {
+                          sets++;
+                          handlerUpdateSets(sets);
+                        });
+                      },
+                    ),
+                    SizedBox(width: 16.0),
+                    IconButton(
+                      icon: Icon(Icons.arrow_downward),
+                      onPressed: () {
+                        setState(() {
+                          if (sets > 0) {
+                            sets--;
+                            handlerUpdateSets(sets);
+                          }
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          );
+        },
+      );
+    },
+  );
 }
